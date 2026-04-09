1e12fd3407858f667035a51a6029326cb07036d5
Plexanov-nik-commits <plexanov-nik@mail.ru>
2026-04-09 22:33:03 +0400

    Initial commit: removed "Hello world" program. A program has been added to calculate a certain Fibonacci number recursively, the sequence begins with (1,1, ...)

--- fib.py
+++ fib.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
-print("Hello, World!")
-
+def fib(n):
+    if n == 1 or n == 2:
+        return 1
+    return fib(n - 1) + fib(n - 2)
+def main():
+    try:
+        n = int(input("введите номер числа Фибоначчи: "))
+        if n <= 0:
+            print("Пожалуйста, введите положительное число")
+            return
+        result = fib(n)
+        print(f"fib({n}) = {result}")
+    except ValueError:
+        print("Пожалуйста, введите целое число")
+if __name__ == "__main__":
+    main()
